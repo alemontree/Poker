55725eb13eee5e28123bfc36b451ef9a84d25cfc
Mikhail <mishnade@gmail.com>
2015-05-18 17:53:35 -0700

    Added some IO. The only thing left undone (besides pretty print) is comparing even hands

--- poker.rb
+++ poker.rb
@@ -1,3 +1,16 @@
+$hand_type = {
+    "1"=> "High Card",
+    "2"=> "Pair",
+    "3"=> "Two Pairs",
+    "4"=> "Three of a Kind",
+    "5"=> "Straight",
+    "6"=> "Flush",
+    "7"=> "Full House",
+    "8"=> "Four of a Kind",
+    "9"=> "Straight Flush",
+    "10"=> "Royal Flush"
+}
+
 class Card
     @@value_dict = {
         T: 10,
@@ -21,23 +34,9 @@ class Card
 end
 
 class Hand
-
-    @@hand_type = {
-        high_card: 1,
-        pair: 2,
-        two_pair: 3,
-        three_of_a_kind: 4,
-        straight: 5,
-        flush: 6,
-        full_house: 7,
-        four_of_a_kind: 8,
-        straight_flush: 9,
-        royal_flush: 10
-    }
-
     def initialize(str)
         @arr = str.split(" ")
-        puts @arr
+
         @hand = []
         @arr.each {|x| @hand.push(Card.new(x)) }
         @hand.sort! {|x, y| x.value <=> y.value }
@@ -49,7 +48,7 @@ class Hand
 
     def is_flush?
         suit_value = @hand[0].suit
-        @hand[1..-1].each {|x|
+        @hand[1..4].each {|x|
             if x.suit != suit_value
                 return false
             end
@@ -67,13 +66,15 @@ class Hand
 
     def repeat_counter
         counter = {}
-        for card in @hand
+        @hand[1..4].each {|card|
+            # if card.is_i
+            #     pass
             if counter.has_key?(card.value)
                 counter[card.value] += 1
             else
                 counter[card.value] = 1
             end
-        end
+        }
         result_array = []
         for k, v in counter
             if v > 1
@@ -85,34 +86,109 @@ class Hand
     end
 
     def hand_assign
+        if @hand.length > 5
+            return @hand
+        end
         flush_check = self.is_flush?
+        straight_check = self.is_straight?
+        repeats = self.repeat_counter
+
+        if flush_check && straight_check && @hand[4].value == 14
+            @hand.push(10)
+        elsif flush_check && straight_check
+            @hand.push(9)
+        elsif flush_check
+            @hand.push(6)
+        elsif straight_check
+            @hand.push(5)
+        elsif repeats == [3,2]
+            @hand.push(7)
+        elsif repeats == [4]
+            @hand.push(8)
+        elsif repeats == [3]
+            @hand.push(4)
+        elsif repeats == [2,2]
+            @hand.push(3)
+        elsif repeats == [2]
+            @hand.push(2)
+        else
+            @hand.push(1)
+        end
+        return @hand
     end
 
 end
 
+def compare(h1, h2)
+    h1_rank = h1.hand_assign[5]
+    h2_rank = h2.hand_assign[5]
+    puts
 
-c1 = Card.new("KC")
-c2 = Card.new("AC")
+    puts "H1: #{$hand_type[h1_rank.to_s]}"
+    puts "H2: #{$hand_type[h2_rank.to_s]}"
+
+    if h1_rank > h2_rank
+        return 1
+    elsif h1_rank < h2_rank
+        return -1
+    else
+        return 0
+    end
+
+end
+
+def get_input
+    #text = File.new("poker.txt")
+    arr = IO.readlines("poker.txt")
+    stats = []
+    for pair in arr
+        h1 = Hand.new(pair[0..14])
+        h2 = Hand.new(pair[15..29])
+        stats.push(compare(h1, h2))
+
+    end
+    puts stats.count(1)
+    puts stats.count(-1)
+    puts stats.count(0)
+
+
+    #puts h2
+    
+
+end
+
+get_input
+
+# c1 = Card.new("KC")
+# c2 = Card.new("AC")
 
 # c1.display_card
 # c2.display_card
 
-h1 = Hand.new("5S 5C 4C 5S 4S")
-h2 = Hand.new("8C 9C TC JC QC")
-h3 = Hand.new("TS JS QC KS AS")
-h4 = Hand.new("8S TS KS 9S 4S")
+# h1 = Hand.new("5S 3C 4C 5S 4S")
+# h2 = Hand.new("8C 9C TC JC QC")
+# h3 = Hand.new("TS JS QS KS AS")
+# h4 = Hand.new("8S TS 8C TD 8H")
 
-h1.show_hand
-h2.show_hand
-h3.show_hand
-h4.show_hand
+# h1.show_hand
+# h2.show_hand
+# h3.show_hand
+# h4.show_hand
 
 
-puts 1, "Flush? #{h1.is_flush?} Straight? #{h1.is_straight?}" #false
-puts 2, "Flush? #{h2.is_flush?} Straight? #{h2.is_straight?}" #true
-puts 3, "Flush? #{h3.is_flush?} Straight? #{h3.is_straight?}" #false
-puts 4, "Flush? #{h4.is_flush?} Straight? #{h4.is_straight?}" #true
+# puts 1, "Flush? #{h1.is_flush?} Straight? #{h1.is_straight?}" #false
+# puts 2, "Flush? #{h2.is_flush?} Straight? #{h2.is_straight?}" #true
+# puts 3, "Flush? #{h3.is_flush?} Straight? #{h3.is_straight?}" #false
+# puts 4, "Flush? #{h4.is_flush?} Straight? #{h4.is_straight?}" #true
 
-puts" result is #{h1.repeat_counter}"
-puts h2.hand_assign
+#puts" result is #{h1.repeat_counter}"
+# puts h1.hand_assign[5] #3
+# puts h2.hand_assign[5] #9
+# puts h3.hand_assign[5] #10
+# puts h4.hand_assign[5] #7
+
+# compare(h3, h2)
+# compare(h3, h4)
+# compare(h1, h2)
+# compare(h3, h2)
 
